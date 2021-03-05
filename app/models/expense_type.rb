dade5bd72efe40fce701e4f19b47b1e6894ad51c
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-05 14:23:38 -0300

    Destroy expenses and expense_type_specification when the expense type is deleted

--- app/models/expense_type.rb
+++ app/models/expense_type.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
 class ExpenseType < ApplicationRecord
-  has_one :expense_type_specification
-  has_many :expenses
+  has_one :expense_type_specification, dependent: :destroy
+  has_many :expenses, dependent: :destroy
 end
