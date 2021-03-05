3667a06149400262231289619ccf2882a82f3682
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-04 22:49:16 -0300

    Test the relation between expense type and expense type specification

--- spec/models/expense_type_spec.rb
+++ spec/models/expense_type_spec.rb
@@ -1,5 +1,14 @@
+# frozen_string_literal: true
+
 require 'rails_helper'
 
 RSpec.describe ExpenseType, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  let(:expense_type) { create(:expense_type) }
+  let!(:expense_type_specification) { create(:expense_type_specification, expense_type: expense_type) }
+
+  describe 'associations' do
+    describe 'belongs_to :expense_type' do
+      it { expect(expense_type.expense_type_specification).to be_instance_of(ExpenseTypeSpecification) }
+    end
+  end
 end
