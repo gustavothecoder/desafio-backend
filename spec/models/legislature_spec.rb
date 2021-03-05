287ff459f07435fd7eb610b00179ab8f19b996a7
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-04 21:47:57 -0300

    Create the legislature spec

---
+++ spec/models/legislature_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+require 'rails_helper'
+
+RSpec.describe Legislature, type: :model do
+  describe 'associations' do
+    describe 'has_many :parliamentarians' do
+      # pending
+    end
+  end
+end
