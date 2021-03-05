55b93dc3b3f33159ed8c711436b819dc66ba24d3
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-04 22:19:12 -0300

    Define the relation between deputy and legislature

---
+++ app/models/deputy.rb
@@ -0,0 +1,5 @@
+# frozen_string_literal: true
+
+class Deputy < ApplicationRecord
+  belongs_to :legislature
+end
--- app/models/legislature.rb
+++ app/models/legislature.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
 class Legislature < ApplicationRecord
+  has_many :deputies
 end
