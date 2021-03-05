f5241e606c14eaecd20c46970f7c26ae7d63d7d4
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-05 15:18:22 -0300

    Add database_cleaner-active_record to the project

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -61,4 +61,9 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+
+  config.before(:suite) do
+    DatabaseCleaner.strategy = :transaction
+    DatabaseCleaner.clean_with(:truncation)
+  end
 end
