c324598228db747d0bec2e1d237bf7391f0370e0
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-06 19:45:04 -0300

    Document BasePresenter

--- app/presenters/base_presenter.rb
+++ app/presenters/base_presenter.rb
@@ -1,5 +1,8 @@
 # frozen_string_literal: true
 
+# This class help us to create presenters.
+# Inherit from SimpleDelegator provides us with the means to delegate
+# all supported method calls to the object passed into the constructor.
 class BasePresenter < SimpleDelegator
   def initialize(object)
     super(object)
@@ -7,6 +10,8 @@ class BasePresenter < SimpleDelegator
 
   private
 
+  # This method acts as an alias for ApplicationController.helpers and it
+  # allows us to use helpers like link_to and content_tag on our presenters.
   def helpers
     ApplicationController.helpers
   end
