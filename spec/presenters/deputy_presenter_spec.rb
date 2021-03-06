341a72660ee4eec034cafdacc1ff3c01f366ccda
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-06 16:04:37 -0300

    Create the deputy_presenter#highest_expense_link tests

--- spec/presenters/deputy_presenter_spec.rb
+++ spec/presenters/deputy_presenter_spec.rb
@@ -27,6 +27,12 @@ RSpec.describe DeputyPresenter, type: :presenter do
     end
   end
 
+  describe '#highest_expense_link' do
+    it 'should return the invoice link' do
+      expect(deputy_presenter.highest_expense_link).to eq('<a target="_blank" href="https://www.example.com">aqui</a>')
+    end
+  end
+
   describe '#total_spend' do
     it 'should return 500.0' do
       expect(deputy_presenter.total_spend).to eq(500.0)
