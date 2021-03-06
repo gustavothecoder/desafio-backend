d983a267058fc3ba323e952f1f74287d4126e341
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-06 10:40:07 -0300

    Create the application_helper#format_time

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -5,4 +5,8 @@ module ApplicationHelper
     fragments = name.split(' ')
     fragments.count == 1 ? fragments.first : "#{fragments.first} #{fragments.last}"
   end
+
+  def format_time(time, format = '%d/%m/%y')
+    time.nil? ? 'Indefinida' : time.strftime(format)
+  end
 end
