09c39e2e3131dc35e7dcf1dea0cb554d0751b77e
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-06 08:09:09 -0300

    Create the application_helper#summarize_name

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,8 @@
+# frozen_string_literal: true
+
 module ApplicationHelper
+  def summarize_name(name)
+    fragments = name.split(' ')
+    fragments.count == 1 ? fragments.first : "#{fragments.first} #{fragments.last}"
+  end
 end
