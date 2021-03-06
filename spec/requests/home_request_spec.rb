138847b2a12ba72fe073404cf4bc9d8c17feebfd
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-06 08:19:42 -0300

    Fix describe

--- spec/requests/home_request_spec.rb
+++ spec/requests/home_request_spec.rb
@@ -3,7 +3,7 @@
 require 'rails_helper'
 
 RSpec.describe HomeController, type: :request do
-  describe 'GET /index' do
+  describe 'GET /' do
     it 'returns http success' do
       get root_path
       expect(response).to have_http_status(:success)
