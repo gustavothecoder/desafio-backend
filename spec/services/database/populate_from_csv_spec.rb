2656cbd816ffe4c655189389a7eb0c3075b8838b
Gustavo Ribeiro <g2_ribeiro@hotmail.com>
2021-03-05 23:03:24 -0300

    Update the Database::PopulateFromCsv tests

--- spec/services/database/populate_from_csv_spec.rb
+++ spec/services/database/populate_from_csv_spec.rb
@@ -3,25 +3,6 @@
 require 'rails_helper'
 
 RSpec.describe Database::PopulateFromCsv, type: :service do
-  describe 'when an error occurs' do
-    before(:all) do
-      csv = Rack::Test::UploadedFile.new('spec/fixtures/bad_csv.csv')
-      @result = Database::PopulateFromCsv.call(csv)
-    end
-
-    it 'should return false' do
-      expect(@result).to be(false)
-    end
-
-    it 'should not create records' do
-      expect(Legislature.count).to eq(0)
-      expect(Deputy.count).to eq(0)
-      expect(ExpenseType.count).to eq(0)
-      expect(ExpenseTypeSpecification.count).to eq(0)
-      expect(Expense.count).to eq(0)
-    end
-  end
-
   context 'when the database is successfully populated ' do
     before(:all) do
       csv = Rack::Test::UploadedFile.new('spec/fixtures/test.csv')
@@ -53,6 +34,47 @@ RSpec.describe Database::PopulateFromCsv, type: :service do
 
     it 'should have two expenses' do
       expect(Deputy.find_by_name('Cacá Leão').expenses.count).to eq(2)
+    end
+
+    it 'should only create the expense type COMBUSTÍVEIS E LUBRIFICANTES' do
+      expect(ExpenseType.count).to eq(1)
+      expect(ExpenseType.first.description).to eq('COMBUSTÍVEIS E LUBRIFICANTES.')
+    end
+
+    it 'should only create the expense type specification Veículos Automotores' do
+      expect(ExpenseTypeSpecification.count).to eq(1)
+      expect(ExpenseTypeSpecification.first.description).to eq('Veículos Automotores')
+    end
+  end
+
+  context 'when there is an error on the second line of the CSV' do
+    before(:all) do
+      Legislature.destroy_all
+      ExpenseType.destroy_all
+      csv = Rack::Test::UploadedFile.new('spec/fixtures/bad_csv.csv')
+      @result = Database::PopulateFromCsv.call(csv)
+    end
+
+    it 'should return false' do
+      expect(@result).to be(false)
+    end
+
+    it 'should only create the 2019 legislature' do
+      expect(Legislature.count).to eq(1)
+      expect(Legislature.first.year).to eq(2019)
+    end
+
+    it 'should only create the deputy Osmar Terra' do
+      expect(Deputy.count).to eq(1)
+      expect(Deputy.first.name).to eq('Osmar Terra')
+    end
+
+    it 'should only create SP deputies' do
+      expect(Deputy.where("state != 'SP'").empty?).to be(true)
+    end
+
+    it 'should have only one expense' do
+      expect(Deputy.find_by_name('Osmar Terra').expenses.count).to eq(1)
     end
 
     it 'should only create the expense type COMBUSTÍVEIS E LUBRIFICANTES' do
